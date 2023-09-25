7d7777af9fe9ce97751d934a3e11094d8c4d0736
ArbaazKhan1 <bazzy_94@yahoo.com>
2023-09-25 19:23:40 -0400

    fixed bugs

--- src/main/java/com/arbaazkhan/servermanager/controller/ServerController.java
+++ src/main/java/com/arbaazkhan/servermanager/controller/ServerController.java
@@ -94,6 +94,6 @@ public class ServerController {
 
     @GetMapping(path = "/image/{fileName}", produces = IMAGE_PNG_VALUE)
     public byte[] getServerImage(@PathVariable("fileName") String fileName) throws IOException {
-        return Files.readAllBytes(Paths.get("src/main/resources/serverImages" + fileName));
+        return Files.readAllBytes(Paths.get("src/main/resources/serverImages/" + fileName));
     }
 }
